bc52abe1173e4c942ea88c5341a1d6ec95200897
Trevor Sather <trevor@squareeye.com>
2026-05-11 08:36:35 +0200

    Membership functions.

--- functions/wordpress.php
+++ functions/wordpress.php
@@ -15,3 +15,47 @@ add_action( 'after_setup_theme', function () {
         )
     );
 } );
+
+/**
+ * Administrators and editors only: others should not use the admin UI.
+ */
+function law_user_may_use_wp_admin() {
+	if ( ! is_user_logged_in() ) {
+		return false;
+	}
+	$user = wp_get_current_user();
+	return (bool) array_intersect( array( 'administrator', 'editor' ), (array) $user->roles );
+}
+
+/**
+ * Front-end admin bar: hide for everyone except admins and editors.
+ */
+add_filter(
+	'show_admin_bar',
+	function ( $show ) {
+		return law_user_may_use_wp_admin();
+	},
+	100
+);
+
+/**
+ * Block wp-admin (including Dashboard) unless admin or editor. AJAX unchanged.
+ */
+add_action(
+	'admin_init',
+	function () {
+		if ( ! is_user_logged_in() ) {
+			return;
+		}
+		if ( wp_doing_ajax() ) {
+			return;
+		}
+		if ( law_user_may_use_wp_admin() ) {
+			return;
+		}
+
+		wp_safe_redirect( home_url( '/' ) );
+		exit;
+	},
+	1
+);
