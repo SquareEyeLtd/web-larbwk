c6c7f9c9cfa29a9443824bbf14f3734c656f9a95
Trevor Sather <trevor@squareeye.com>
2026-06-16 13:35:55 +0200

    Rename button.

--- functions/gravity-flow.php
+++ functions/gravity-flow.php
@@ -1,8 +1,11 @@
 <?php
 	
-	add_filter( 'gettext_gravityflow', function ( $translation, $text, $domain ) {
+add_filter( 'gettext_gravityflow', function ( $translation, $text, $domain ) {
 	if ( 'Revert' === $text ) {
 		return 'Send back';
+	}
+	if ( 'Approve' === $text ) {
+		return 'Proceed';
 	}
 	if ( 'Are you sure you want to revert this entry?' === $text ) {
 		return 'Are you sure you want to send this back?';
