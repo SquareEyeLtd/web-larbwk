0729ac01402538de0aeb42c1de8842e6a75f985b
Denis <gusevdigital@gmail.com>
2026-09-02 11:58:36 +0200

    Remove backfilling helper

--- functions.php
+++ functions.php
@@ -24,4 +24,3 @@ require_once(get_theme_file_path('/functions/calendar.php'));
 require_once(get_theme_file_path('/functions/banner-account-status.php'));
 require_once(get_theme_file_path('/functions/migrate-speakers.php'));
 
-require_once(get_theme_file_path('/functions/backfill-user-country.php'));
